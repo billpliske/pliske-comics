2bc6ab5a6485657762b51d21f5c996babda7e293
Bill Pliske <bpliske@gmail.com>
2019-01-23 14:44:09 -0700

    updates

--- client/src/pages/Comics.js
+++ client/src/pages/Comics.js
@@ -11,9 +11,9 @@ const Comics = () => {
             <Header header="Comic collection" />
             <Intro>
                 Instead of selling my comics one by one through something like eBay, I've decided to
-                just sell the entire collection at once. The rankings are simply my opinion. I tried
-                being brutally honest, but admittedly, I could be off in either direction as I'm not
-                an official grader.
+                just sell the entire collection at once. The condition for each is simply my
+                opinion. I tried being brutally honest, but admittedly, I could be off in either
+                direction, as I'm not a professional grader.
             </Intro>
             <ComicSearch />
         </Wrapper>
